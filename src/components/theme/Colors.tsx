3061e1090c1b06ac8bd2c5ea1fc67e74a97bbfb2
yunhee_shin <qlyh8@naver.com>
2022-02-09 17:42:53 +0900

    색상 추가

--- src/components/theme/Colors.tsx
+++ src/components/theme/Colors.tsx
@@ -14,19 +14,20 @@ const Container = (props: ContainerProps) => {
 const Item = (props: ItemProps) => {
   const { title, colors } = props;
   return (
-    <Box>
+    <Box sx={{ width: 300 }}>
       <Typography variant="h4" sx={{ my: 2 }}>
         {title}
       </Typography>
       <Box>
-        {Object.entries(colors).map(([name, color]) => {
-          return (
-            <Box key={name} sx={{ display: 'flex', alignItems: 'center' }}>
-              <Typography sx={{ width: 300 }}>{`${name} (${color})`}</Typography>
-              <Box sx={{ width: 50, height: 50, backgroundColor: color }} />
-            </Box>
-          );
-        })}
+        {colors &&
+          Object.entries(colors).map(([name, color]) => {
+            return (
+              <Box key={name} sx={{ display: 'flex', alignItems: 'center' }}>
+                <Typography sx={{ width: 250 }}>{`${name} (${color})`}</Typography>
+                <Box sx={{ width: 50, height: 50, backgroundColor: color }} />
+              </Box>
+            );
+          })}
       </Box>
     </Box>
   );
@@ -36,7 +37,12 @@ export const Colors = () => {
   return (
     <Stack direction="column" alignItems="stretch" spacing={2}>
       <Container>
+        <Item title="Global" colors={colors.global} />
+        <Item title="Chart" colors={colors.chart} />
+      </Container>
+      <Container>
         <Item title="Button" colors={colors.button} />
+        <Item />
       </Container>
     </Stack>
   );
@@ -47,6 +53,6 @@ interface ContainerProps {
 }
 
 interface ItemProps {
-  title: string;
-  colors: { [name: string]: string };
+  title?: string;
+  colors?: { [name: string]: string };
 }
--- src/typings/@mui/index.d.ts
+++ src/typings/@mui/index.d.ts
@@ -2,19 +2,45 @@ import '@material-ui/core/styles';
 
 declare module '@mui/material/styles/createPalette' {
   interface Palette {
+    global: {
+      bg100: string;
+      bgDark100: string;
+      danger100: string;
+    };
+    chart: {
+      blue100: string;
+      blue300: string;
+      green400: string;
+      black400: string;
+      black500: string;
+    };
     button: {
       primaryBg: string;
       primaryHoverBg: string;
       disabled: string;
       disabledBg: string;
+      dangerBg: string;
     };
   }
   interface PaletteOptions {
+    global: {
+      bg100: string;
+      bgDark100: string;
+      danger100: string;
+    };
+    chart: {
+      blue100: string;
+      blue300: string;
+      green400: string;
+      black400: string;
+      black500: string;
+    };
     button: {
       primaryBg: string;
       primaryHoverBg: string;
       disabled: string;
       disabledBg: string;
+      dangerBg: string;
     };
   }
 }
