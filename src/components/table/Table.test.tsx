ba852b4ad1486abc59afc8b8e78d5c021e573f34
이경준 <gyoungjun_lee@tmax.co.kr>
2022-02-17 11:14:17 +0900

    [test] snapshot, 빈 배열 받아왔을 때 emptybox보여주는 테스트 추가

---
+++ src/components/table/Table.test.tsx
@@ -0,0 +1,41 @@
+import { rest } from 'msw';
+import * as React from 'react';
+import { render, screen } from '../../../test-utils';
+import { server } from '../../mocks/server';
+import Table from './Table';
+import { ServiceModel } from '../../models/index';
+
+describe('<Table /> snapshot test', () => {
+  test('table render', () => {
+    const { container } = render(
+      <Table
+        tableItems={[
+          { name: 'name', displayTitle: 'Name', className: '' },
+          { name: 'namespace', displayTitle: 'Namespace', className: '' },
+        ]}
+        kindObj={ServiceModel}
+      />,
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
+
+test('when fetching product datas, face an error', async () => {
+  server.use(
+    rest.get('http://localhost:6006/api/kubernetes/api/v1/services', (req, res, ctx) => {
+      return res.once(ctx.status(200), ctx.json({ items: [] }));
+    }),
+  );
+  render(
+    <Table
+      tableItems={[
+        { name: 'name', displayTitle: 'Name', className: '' },
+        { name: 'namespace', displayTitle: 'Namespace', className: '' },
+      ]}
+      kindObj={ServiceModel}
+    />,
+  );
+
+  const emptyBox = await screen.findByTestId('empty-box');
+  expect(emptyBox).toHaveTextContent('해당 리소스를 찾을 수 없습니다.');
+});
