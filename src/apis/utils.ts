688b2c09e2befb14ca2de3692cd13f2ebb9246ed
이경준 <gyoungjun_lee@tmax.co.kr>
2022-02-18 11:20:52 +0900

    [pr] getProperUrl 함수 파일 분리 및 고도화

---
+++ src/apis/utils.ts
@@ -0,0 +1,20 @@
+import { K8sModelType } from '../models';
+
+export function getProperUrl(model: K8sModelType, requestType: RequestType) {
+  const { apiVersion, plural } = model;
+
+  switch (requestType) {
+    case 'list': {
+      return `/api/kubernetes/api/${apiVersion}/${plural}`;
+    }
+    default: {
+      throw Error('url을 찾을 수 없습니다.');
+    }
+  }
+}
+
+export enum RequestType {
+  LIST = 'list',
+  CREATE = 'create',
+  DETAIL = 'detail',
+}
