ae025810a45b6d39e393207d5fcd9ac262d2dfd6
yunhee_shin <qlyh8@naver.com>
2022-03-03 13:50:50 +0900

    [feat] panel 순서 수정 및 테스트 아웃풋 업데이트

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   staticDirs: ['../public'],
-  addons: ['@storybook/addon-jest', '@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-create-react-app'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-jest', '@storybook/preset-create-react-app'],
   framework: '@storybook/react',
   core: {
     builder: 'webpack5',
