fea0188e2cda84f99a5f5c0e919c3f2ef28c7cea
yunhee_shin <qlyh8@naver.com>
2022-03-07 16:14:41 +0900

    test commit

--- src/components/autocomplete/Autocomplete.tsx
+++ src/components/autocomplete/Autocomplete.tsx
@@ -52,7 +52,6 @@ export interface AutocompleteProps {
   onChange?: (event: React.SyntheticEvent, value: any, reason: AutocompleteChangeReason, details?: AutocompleteChangeDetails<string>) => void;
   /**
    * Callback fired when the popup requests to be closed.
-   * Use in controlled mode (see open).
    *
    * @param {React.SyntheticEvent} event The event source of the callback.
    * @param {string} reason Can be: `"toggleInput"`, `"escape"`, `"selectOption"`, `"removeOption"`, `"blur"`.
