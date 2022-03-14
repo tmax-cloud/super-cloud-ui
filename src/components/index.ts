74379c1838422c3208efe654819f5391c81e01a5
yunhee_shin <qlyh8@naver.com>
2022-03-14 15:32:33 +0900

    [feat] Select 컴포넌트 구현

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 export * from './autocomplete';
+export * from './select';
 export { default as Button } from './button/Button';
-export { SimpleSelect } from './select/Select';
-export { MultipleSelect } from './select/MultipleSelect';
 export { default as Table } from './table/Table';
 export { default as TextField } from './textfield/TextField';
 export { default as Tooltip } from './tooltip/Tooltip';
