c172aec74add7f6ca86fb9ef40707ef070f7658e
이경준 <gyoungjun_lee@tmax.co.kr>
2022-03-14 13:21:01 +0900

    [pr] 사용안되는 로그 제거

--- src/components/table/TableRow.tsx
+++ src/components/table/TableRow.tsx
@@ -10,7 +10,6 @@ export const TableRow = (tableItems: any, columnDataList: TableItemProps[]) =>
       {columnDataList.map((currentColumnItem: TableItemProps, columnIdx: number) => {
         const isKebab = currentColumnItem.name === 'kebab';
         const kebabItems = currentColumnItem.customKebabItems ? [...currentColumnItem.customKebabItems] : [];
-        console.log('kebabItems: ', kebabItems);
         return (
           <TableCell key={currentColumnItem.name} data-testid={`${rowIdx}_${columnIdx}_cell`}>
             {isKebab ? <Kebab kebabItems={kebabItems} /> : getTableValue(item, currentColumnItem)}
