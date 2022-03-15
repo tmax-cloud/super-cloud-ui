f6539adfd99c95ab5e2d6b3459ccce5feabd92ee
yunhee_shin <qlyh8@naver.com>
2022-03-15 15:46:53 +0900

    사용하지 않는 코드 제거 및 useEffect 수정

--- src/hooks/document-listener.ts
+++ src/hooks/document-listener.ts
@@ -16,48 +16,41 @@ const textInputKeyHandler = {
 };
 
 export const useDocumentListener = <T extends HTMLElement>(keyEventMap: KeyEventMap = textInputKeyHandler) => {
-  const [visible, setVisible] = React.useState(true);
   const ref = React.useRef<T>(null);
 
-  const handleEvent = (e: MouseEvent) => {
-    if (!ref?.current?.contains(e.target as T)) {
-      setVisible(false);
-    }
-  };
-
-  const handleKeyEvents = (e: KeyboardEvent) => {
-    const { nodeName } = e.target as T;
-    switch (keyEventMap[e.key]) {
-      case KeyEventModes.HIDE:
-        setVisible(false);
-        ref.current?.blur();
-        break;
-      case KeyEventModes.FOCUS:
-        if (
-          document.activeElement !== ref.current &&
-          // Don't steal focus if the user types the focus shortcut in another text input.
-          nodeName !== 'INPUT' &&
-          nodeName !== 'TEXTAREA'
-        ) {
-          ref.current?.focus();
-          e.preventDefault();
-        }
-        break;
-      default:
-        break;
-    }
-  };
+  const handleKeyEvents = React.useCallback(
+    (e: KeyboardEvent) => {
+      const { nodeName } = e.target as T;
+      switch (keyEventMap[e.key]) {
+        case KeyEventModes.HIDE:
+          ref.current?.blur();
+          break;
+        case KeyEventModes.FOCUS:
+          if (
+            document.activeElement !== ref.current &&
+            // Don't steal focus if the user types the focus shortcut in another text input.
+            nodeName !== 'INPUT' &&
+            nodeName !== 'TEXTAREA'
+          ) {
+            ref.current?.focus();
+            e.preventDefault();
+          }
+          break;
+        default:
+          break;
+      }
+    },
+    [keyEventMap],
+  );
 
   React.useEffect(() => {
-    document.addEventListener('click', handleEvent, true);
     document.addEventListener('keydown', handleKeyEvents, true);
     return () => {
-      document.removeEventListener('click', handleEvent, true);
       document.removeEventListener('keydown', handleKeyEvents, true);
     };
-  });
+  }, [handleKeyEvents]);
 
-  return { visible, setVisible, ref };
+  return { ref };
 };
 
 export type KeyEventMap = {
